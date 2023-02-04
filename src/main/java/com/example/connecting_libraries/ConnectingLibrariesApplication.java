ab66ab7a17a7a7a70b643f9896891acff4168dad
Polyxen1a <maslowa@inbox.ru>
2023-02-04 22:57:45 +0300

    Initial commit

---
+++ src/main/java/com/example/connecting_libraries/ConnectingLibrariesApplication.java
@@ -0,0 +1,13 @@
+package com.example.connecting_libraries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectingLibrariesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectingLibrariesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/connecting_libraries/ConnectingLibrariesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.connecting_libraries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectingLibrariesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
